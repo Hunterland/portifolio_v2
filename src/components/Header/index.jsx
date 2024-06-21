bf172cda27ce64712de3d848a0dff07ea98c54ce
Alan Barroncas <alanbarroncas@gmail.com>
2024-06-21 15:11:28 -0400

    atualizando Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,15 @@
 // importando estilos //
 // estrutura HTML e CSS //
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
 import logo from "../../assets/logo_AB.svg";
 import { Link } from "react-router-dom";
-import { useState } from 'react'
 
 function Header() {
-
   const [ showMenu, setShowMenu ] = useState(false)
   const toggleMenu = () => {
-    setShowMenu(!showMenu)
+    setShowMenu(!showMenu);
   }
 
   return (
@@ -20,17 +18,18 @@ function Header() {
       <Link to="/" className={styles.logoLink}>
         <img src={logo} alt="Logo" className={styles.logo} />
       </Link>
-
+      {/* Navegação */}
       <nav className={`${styles.menuSandwich} ${ showMenu ? styles.show : '' }`} onClick={toggleMenu}>
         <Link to="/" className="active" href="">Home</Link>
         <Link to="/sobre" href="">Sobre</Link>
         <Link to="/projetos" href="">Projetos</Link>
         <Link to="/contatos" href="">Contatos</Link>
       </nav>
+      {/* Menu Mobile */}
       <div className={styles.menuButton} onClick={toggleMenu}>
-        <span className={styles.linha}></span>
-        <span className={styles.linha}></span>
-        <span className={styles.linha}></span>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
       </div>
     </header>
   );
