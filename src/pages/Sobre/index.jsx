8e39c1e91f0c3b86d26c234e5f956f696bd64b2a
Alan Barroncas <alanbarroncas@gmail.com>
2024-06-25 17:26:01 -0400

    subindo alterações

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,9 +1,10 @@
 // importe components //
-
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 // import css//
 import styles from "./Sobre.module.css";
 // import avatar //
-import avatar from "./images/avatar.png";
+import avatar from "./images/";
 
 // icones //
 import figma from "./images/icon-figma.svg";
@@ -18,43 +19,58 @@ import sql from "./images/icon-sql.svg";
 function Sobre() {
   return (
     <section className={styles.sobre}>
-      <div className={styles.bio}>
+      <div className={styles.profileSection}>
         <img src={avatar} alt="Alan Barroncas" className={styles.avatar} />
-        <div className={styles.textos}>
+        <div className={styles.textSection}>
           <h2>Sobre Mim</h2>
-            <p>
-              Apaixonado por inovação digital e sempre em busca de novos desafios,
-              atualmente estou aprimorando minhas habilidades no curso de Análise e Desenvolvimento de Sistemas.
-              Com 2 anos de experiência prática como Desenvolvedor Frontend | Fullstack,
-              possuo competências tanto em desenvolvimento web quanto em software.
-            </p>
-            <br />
-            <p>
-              Além disso, tive uma breve incursão na área de Suporte,
-              o que contribuiu para o desenvolvimento da minha capacidade de compreensão
-              e resolução de problemas cotidianos. Tenho habilidades de adaptação,
-              comunicação eficaz e trabalho em equipe.
-            </p>
-            <br />
-            <p>
-              Estou sempre buscando oportunidades que me permitam crescer tanto profissional quanto pessoalmente.
-              Adoto uma abordagem holística que valoriza a integração entre tecnologia, criatividade e impacto social.
-              Estou aberto a conexões e oportunidades para colaborar em projetos que fazem a diferença.
-            </p>
+          <p>
+            Com 2 anos de experiência prática como Desenvolvedor Frontend /
+            Fullstack, possuo competências tanto em desenvolvimento web quanto
+            em software.
+          </p>
+          <br />
+          <p>
+            Minha jornada profissional também inclui uma experiência breve, mas
+            extremamente valiosa, na área de Suporte Técnico. Essa vivência me
+            ajudou a desenvolver uma capacidade de resolução de problemas e me
+            proporcionou uma compreensão mais profunda dos desafios cotidianos
+            enfrentados pelos usuários. Sou uma pessoal altamente adaptável, que
+            se comunica e trabalha muito bem em equipe.
+          </p>
+          <br />
+          <p>
+            Estou sempre buscando oportunidades que me permitam crescer tanto
+            profissional quanto pessoalmente. Adoto uma abordagem holística que
+            valoriza a integração entre tecnologia, criatividade e impacto
+            social. Estou aberto a conexões e oportunidades para colaborar em
+            projetos que podem fazer a diferença.
+          </p>
         </div>
       </div>
 
-      <div className={styles.techs}>
-        <h3>Techs</h3>
-        <div className={styles.icones}>
-          <img className={styles.icon1} src={figma} alt="Figma" />
-          <img className={styles.icon2} src={typescript} alt="Ts" />
-          <img className={styles.icon3} src={js} alt="Js" />
-          <img className={styles.icon4} src={git} alt="Git" />
-          <img className={styles.icon5} src={nodejs} alt="NodeJs" />
-          <img className={styles.icon6} src={angular} alt="Angular" />
-          <img className={styles.icon7} src={react} alt="Reacr" />
-          <img className={styles.icon8} src={sql} alt="Sql" />
+      {/* Área de Hardskills */}
+
+      <div className={styles.skillsSection}>
+        <div className={styles.hardskills}>
+          <h3>Hardskills</h3>
+          <div className={styles.icones}>
+            <img className={styles.icon1} src={figma} alt="Figma" />
+            <img className={styles.icon2} src={typescript} alt="Ts" />
+            <img className={styles.icon3} src={js} alt="Js" />
+            <img className={styles.icon4} src={git} alt="Git" />
+            <img className={styles.icon5} src={nodejs} alt="NodeJs" />
+            <img className={styles.icon6} src={angular} alt="Angular" />
+            <img className={styles.icon7} src={react} alt="Reacr" />
+            <img className={styles.icon8} src={sql} alt="Sql" />
+          </div>
+        </div>
+        <div className={styles.softskills}>
+          <h3>Softskills</h3>
+          <ul>
+            <li>Adaptabilidade</li>
+            <li>Comunicação</li>
+            <li>Trabalho em equipe</li>
+          </ul>
         </div>
       </div>
     </section>
