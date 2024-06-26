c5a74157e97d70f76aeba5a998bcdf3974a38126
Alan Barroncas <alanbarroncas@gmail.com>
2024-06-26 17:07:59 -0400

    atualizando icones skills

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -9,8 +9,12 @@ import typescript from "./images/icon-ts.svg";
 import js from "./images/icon-js.svg";
 import git from "./images/icon-git.svg";
 import nodejs from "./images/icon-node.svg";
+import docker from "./images/icon-dk.svg";
+import bootstrap from "./images/icon-bs.svg";
 import angular from "./images/icon-angular.svg";
 import react from "./images/icon-react.svg";
+import tailwind from "./images/icon-tw.svg";
+import php from "./images/icon-php.svg";
 import sql from "./images/icon-sql.svg";
 
 function Sobre() {
@@ -28,10 +32,10 @@ function Sobre() {
           <br />
           <p>
             Minha jornada profissional também inclui uma experiência breve, mas
-            extremamente valiosa, na área de Suporte Técnico. Essa vivência me
+            extremamente valiosa, na Área de Suporte Técnico. Essa vivência me
             ajudou a desenvolver uma capacidade de resolução de problemas e me
             proporcionou uma compreensão mais profunda dos desafios cotidianos
-            enfrentados pelos usuários. Sou uma pessoal altamente adaptável, que
+            enfrentados pelos usuáios. Sou uma pessoal altamente adaptável, que
             se comunica e trabalha muito bem em equipe.
           </p>
           <br />
@@ -39,7 +43,7 @@ function Sobre() {
             Estou sempre buscando oportunidades que me permitam crescer tanto
             profissional quanto pessoalmente. Adoto uma abordagem holística que
             valoriza a integração entre tecnologia, criatividade e impacto
-            social. Estou aberto a conexões e oportunidades para colaborar em
+            social. Estou aberto a conexães e oportunidades para colaborar em
             projetos que podem fazer a diferença.
           </p>
         </div>
@@ -50,14 +54,23 @@ function Sobre() {
         <div className={styles.hardskills}>
           <h3>Hardskills</h3>
           <div className={styles.icones}>
-            <img className={styles.icon1} src={figma} alt="Figma" />
-            <img className={styles.icon2} src={typescript} alt="Ts" />
-            <img className={styles.icon3} src={js} alt="Js" />
-            <img className={styles.icon4} src={git} alt="Git" />
-            <img className={styles.icon5} src={nodejs} alt="NodeJs" />
-            <img className={styles.icon6} src={angular} alt="Angular" />
-            <img className={styles.icon7} src={react} alt="React" />
-            <img className={styles.icon8} src={sql} alt="Sql" />
+            <div className={styles.row}>
+              <img className={styles.icon1} src={figma} alt="Figma" />
+              <img className={styles.icon2} src={typescript} alt="Ts" />
+              <img className={styles.icon3} src={js} alt="Js" />
+              <img className={styles.icon4} src={git} alt="Git" />
+              <img className={styles.icon5} src={nodejs} alt="NodeJs" />
+              <img className={styles.icon6} src={docker} alt="Docker" />
+            </div>
+
+            <div className={styles.row}>
+              <img className={styles.icon7} src={bootstrap} alt="Bootstrap" />
+              <img className={styles.icon8} src={angular} alt="Angular" />
+              <img className={styles.icon9} src={react} alt="React" />
+              <img className={styles.icon10} src={tailwind} alt="Tailwind" />
+              <img className={styles.icon11} src={php} alt="Php" />
+              <img className={styles.icon12} src={sql} alt="Sql" />
+            </div>
           </div>
         </div>
         <div className={styles.softskills}>
